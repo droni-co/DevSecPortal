7aadd43bc45deabb01716bd249ea17e0d330c92b
Gustavo Barragan <dev@droni.co>
2024-07-09 09:48:08 -0500

    main layout base

---
+++ components/MainMenu.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="max-w-[50] bg-slate-300">
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/about">About</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <slot />
+  <div class="flex h-screen">
+    <LazyMainMenu />
+    <main class="flex-1 bg-slate-100">
+      <slot />
+    </main>
   </div>
 </template>
